dcf1878fbe24440013b8c3364fcf145034a66687
zdong_coding <504635366@qq.com>
2017-11-05 16:47:27 +0800

    artifactId

--- app/src/test/java/com/github/zdongcoding/app/ExampleUnitTest.java
+++ app/src/test/java/com/github/zdongcoding/app/ExampleUnitTest.java
@@ -2,6 +2,10 @@ package com.github.zdongcoding.app;
 
 import org.junit.Test;
 
+
+import rx.Observable;
+import rx.Subscriber;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +17,31 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public  void  RxJavaCreate(){
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                System.out.println("subscriber = [" + subscriber + "]");
+            }
+        });
+        observable.subscribe(new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("ExampleUnitTest.onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+        });
     }
 }
